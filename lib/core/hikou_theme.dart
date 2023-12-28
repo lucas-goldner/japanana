e33745cf871f731e85224fd489686fe67ae24d4f
lucas-goldner <lucas.goldner@googlemail.com>
2023-12-28 18:17:12 +0900

    refactor: Move files into own space

---
+++ lib/core/hikou_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HikouTheme {
+  static final theme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+  );
+}
