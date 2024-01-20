b8d397359db18d9b4f9f992d5e060beef22a3268
lucas-goldner <lucas.goldner@googlemail.com>
2024-01-21 04:02:36 +0900

    fix: Remove unused imports

--- lib/features/in_review/presentation/in_review.dart
+++ lib/features/in_review/presentation/in_review.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hikou/core/application/lecture_provider.dart';
 import 'package:hikou/core/domain/lecture.dart';
 import 'package:hikou/core/extensions.dart';
