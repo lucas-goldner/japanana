5675dc7b6cbed8592c0610009be5187232eb0a06
lucas-goldner <lucas.goldner@googlemail.com>
2024-04-07 23:47:57 +0200

    feat: Add more translations for next features

--- lib/features/in_review/presentation/in_review.dart
+++ lib/features/in_review/presentation/in_review.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:japanana/core/application/lecture_provider.dart';
 import 'package:japanana/core/domain/lecture.dart';
 import 'package:japanana/core/extensions.dart';
@@ -9,7 +10,6 @@ import 'package:japanana/core/router.dart';
 import 'package:japanana/features/in_review/presentation/widgets/lecture_card.dart';
 import 'package:japanana/features/review_selection/domain/review_sections.dart';
 import 'package:japanana/features/review_setup/domain/review_setup_options.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 
